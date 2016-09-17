33bec1e87c12a48424ad53e345ce866efd97e7e9
Cihan <cihannisan@gmail.com>
2016-09-16 22:21:56 -0700

    ignore build folder

---
+++ scripts/main.js
@@ -0,0 +1,18 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+var StorePicker = React.createClass({
+
+  render: function() {
+    return (
+    <div>
+      <p>Hello</p>
+      <p>Hello</p>
+      <p>Hello</p>
+      <p>Hello</p>
+    </div>
+    )
+  }
+});
+
+ReactDOM.render(<StorePicker />, document.getElementById('main'));
